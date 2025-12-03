a4bccc32e21134264d61e689d44eef47f96c5bab
riri7282 <rxt7282@nyu.edu>
2025-12-03 17:16:38 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function showCaption(){
+  document.getElementById("profilepic").style.display = "block";
+}
+
+function hideCaption(){
+    document.getElementById("profilepic").style.display = "none";
+}
